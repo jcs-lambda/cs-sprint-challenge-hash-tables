53b70897b0b05bee5855192f3bda76c106a4ce88
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-07-10 10:53:55 -0500

    implements ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,13 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    ht = {}
+
+    for i, x in enumerate(weights):
+        difference = limit - x
+        if difference in ht:
+            return (i, ht[difference])
+        else:
+            ht[x] = i
 
     return None
